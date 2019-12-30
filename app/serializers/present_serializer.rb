7aa4a956834c5d143f960fbaabf0a537ac748130
LimitToInfinity <21stLimitless@gmail.com>
2019-12-30 14:02:14 -0700

    Updates Ratings in PresentSerializer to only include stars and description.

--- app/serializers/present_serializer.rb
+++ app/serializers/present_serializer.rb
@@ -1,4 +1,10 @@
 class PresentSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :image, :ratings
+
+  attribute :ratings do |present|
+    present.ratings.map do |rating|
+      {stars: rating.stars, description: rating.description}
+    end
+  end
 end
